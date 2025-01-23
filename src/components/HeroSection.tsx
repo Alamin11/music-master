10537be796719a92db661580feb20520e9f397b9
Alamin11 <alamincsecu@gmail.com>
2025-01-23 17:50:20 +0600

    added course card , contact section and fixed routing issues

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,7 +4,7 @@ import { Button } from "./ui/moving-border";
 
 const HeroSection = () => {
   return (
-    <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py10 md:py-0">
+    <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
       <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-10 lg:left-80 lg:-top-32"
         fill="white"
@@ -14,10 +14,10 @@ const HeroSection = () => {
           Mastert the Art of Music
         </h1>
         <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-          porro quibusdam esse nam expedita officia eum nihil inventore
-          architecto? Cupiditate autem tempora voluptatibus, esse molestiae amet
-          labore minima magnam delectus.
+          The art of a music school lies in nurturing creativity, honing musical
+          talent, fostering discipline, and inspiring passion. It bridges
+          technical skill with emotional expression, creating well-rounded,
+          confident musicians.
         </p>
         <div className="mt-8">
           <Link href={"/courses"}>
